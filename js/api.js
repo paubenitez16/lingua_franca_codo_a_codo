51c22f063436974421a5803f0bdf9ddc62d3be09
paubenitez16 <111363303+paubenitez16@users.noreply.github.com>
2022-10-23 20:09:59 -0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,17 +1,54 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
-		'X-RapidAPI-Host': 'uk-address-and-postcodes.p.rapidapi.com'
-	}
-};
+	const encodedParams = new URLSearchParams();
+	encodedParams.append("q", "English is hard, but detectably so");
+	
+	const options = {
+		method: 'POST',
+		headers: {
+			'content-type': 'application/x-www-form-urlencoded',
+			'Accept-Encoding': 'application/gzip',
+			'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+			'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+		},
+		body: encodedParams
+	};
+	
+	fetch('https://google-translate1.p.rapidapi.com/language/translate/v2/detect', options)
+		.then(response => response.json())
+		.then(response => console.log(response))
+		.catch(err => console.error(err));
 
-fetch("https://uk-address-and-postcodes.p.rapidapi.com/rapidapi/v1/autocomplete/addresses?query=topstreet")
+		const options = {
+			method: 'GET',
+			headers: {
+				'Accept-Encoding': 'application/gzip',
+				'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+				'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+			}
+		};
+		
+		fetch('https://google-translate1.p.rapidapi.com/language/translate/v2/languages', options)
+			.then(response => response.json())
+			.then(response => console.log(response))
+			.catch(err => console.error(err));
 
-.then(Response console.log(Response))
-
-.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-
-
+			const encodedParams = new URLSearchParams();
+			encodedParams.append("q", "Hello, world!");
+			encodedParams.append("target", "es");
+			encodedParams.append("source", "en");
+			
+			const options = {
+				method: 'POST',
+				headers: {
+					'content-type': 'application/x-www-form-urlencoded',
+					'Accept-Encoding': 'application/gzip',
+					'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+					'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+				},
+				body: encodedParams
+			};
+			
+			fetch('https://google-translate1.p.rapidapi.com/language/translate/v2', options)
+				.then(response => response.json())
+				.then(response => console.log(response))
+				.catch(err => console.error(err));
+				
